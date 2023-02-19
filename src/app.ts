0777fa1989cf472021de4b806497529b5629a3b3
Vova <bordiian.v@gmail.com>
2023-02-19 19:17:42 +0200

    module completed

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,37 @@
-const button = document.querySelector('button')
-function clickHandler(message: string) {
-  console.log('clicked! ' + message)
+//  const userName = 'Max'
+//  let age = 30
+//  age = 20
+
+//  const add = (a: number, b: number = 1) =>  a +b
+
+//  const printOutput: (a: number | string) => void = output => console.log(output)
+
+//  const button = document.querySelector('button')
+//  if (button) {
+//   button.addEventListener('click', event => console.log(event))
+//  }
+//  printOutput(add(5))
+
+ const hobbies = ['Sports', 'Cookies']
+ const activeHobbies = ['Hiking']
+activeHobbies.push(...hobbies)
+
+const person = {
+  firstName: 'Vova',
+  age: 30
 }
 
-function add(n1: number, n2: number) {
-  if (n1 + n2 > 0) {
-    return n1 + n2
-  }
-  return
+const anotherPerson = {...person}
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue
+  }, 0)
 }
 
-if (button) {
-  button.addEventListener('click', clickHandler.bind(null, 'text'))
-}
+const addedNumbers = add(5, 10, 7, 7)
+console.log(addedNumbers)
+
+const [hobbie1, hobbie2] = hobbies
+const { firstName: userName, age } = person
+console.log(userName, age)
