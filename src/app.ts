80de99441b3253d7c1f8d49b14e32969488ff108
Vova <bordiian.v@gmail.com>
2023-02-23 18:02:59 +0200

    configured eslint

--- src/app.ts
+++ src/app.ts
@@ -1,128 +1,32 @@
-type Admin = {
-  name: string
-  privileges: string[]
+// const names: Array<string> = []
+
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('this is done')
+//   }, 2000)
+// })
+
+// promise.then(data => {
+//   data.split(' ')
+// })
+
+function merge<T extends object, U extends object> (objA: T, objB: U) {
+  return Object.assign(objA, objB)
 }
 
-type Employee = {
-  name:  string
-  startDate: Date
+const mergedObj = merge({ name: 'Max' }, { age: 30 })
+console.log(mergedObj)
+
+interface Lengthy {
+  length: number
 }
 
-type ElevatedEmployee = Admin & Employee
-
-const e1: ElevatedEmployee = {
-  name: 'Vova',
-  privileges: ['create-server'],
-  startDate: new Date()
-}
-
-type Combinable = string | number
-type Numeric = number | boolean
-
-type Universal = Combinable & Numeric
-
-function add(a: number, b: number): number
-function add(a: string, b: string): string
-function add(a: Combinable, b: Combinable) {
-  if (typeof a === 'string' || typeof b === 'string') {
-    return a.toString() + b.toString()
+function countAndDescribe<T extends Lengthy> (element: T): [T, string] {
+  let descriptionText = 'Got No value'
+  if (element.length > 0) {
+    descriptionText = 'Got ' + element.length + 'elements'
   }
-  return a + b
+  return [element, descriptionText]
 }
 
-const result = add('lol', 'max')
-result.split(' ')
-
-const fetchUserData = {
-  id: 'u1',
-  name: 'Max',
-  job: { title: 'CEO', description: 'My Own Company' }
-}
-
-console.log(fetchUserData?.job?.title)
-
-// type UnknownEmployee = Employee | Admin
-
-// function printEmployeeinfo(emp: UnknownEmployee) {
-//   console.log('Name: ' + emp.name)
-//   if ('privileges' in emp) {
-//     console.log('Privileges: ' + emp.privileges)
-//   }
-//   if ('startDate' in emp) {
-//     console.log('startDate: ' + emp.startDate)
-//   }
-// }
-
-// printEmployeeinfo(e1)
-
-// class Car  {
-//   drive() {
-//     console.log('Driving')
-//   }
-// }
-
-// class Truck {
-//   drive() {
-//     console.log('Driving a truck')
-//   }
-
-//   loadCargo(amount: number) {
-//     console.log('Loading cargo...' + amount)
-//   }
-// }
-
-// type Vehicle = Car | Truck
-
-// const v1 = new Car()
-// const v2 = new Truck()
-
-// function useVehicle(vehicle: Vehicle) {
-//   vehicle.drive()
-//   if (vehicle instanceof Truck) {
-//     vehicle.loadCargo(1000)
-//   }
-// }
-
-// useVehicle(v1)
-// useVehicle(v2)
-
-// interface Bird {
-//   type: 'bird'
-//   flyingSpeed: number
-// }
-
-// interface Horse {
-//   type: 'horse'
-//   groundSpeed: number
-// }
-
-// type Animal = Bird | Horse
-
-// function moveAnimal(animal: Animal) {
-//   let speed
-//   switch (animal.type) {
-//     case 'bird':
-//       speed = animal.flyingSpeed
-//       break
-//     case 'horse':
-//       speed = animal.groundSpeed
-//       break
-//   }
-//   console.log('Moving with speed: ' + speed)
-// }
-
-// // const userInputElement = <HTMLInputElement>document.getElementById('user-input')
-
-// const userInputElement = document.getElementById('user-input')
-// if (userInputElement) {
-//   (userInputElement as HTMLInputElement).value = 'Hi'
-// }
-
-// interface ErrorContainer { // { email: 'Not a valid email', username: 'Must start with a character'}
-//   [prop: string]: string
-// }
-
-// const errorBag: ErrorContainer = {
-//   email: 'Not a valid email',
-//   user: 'Not a valid username'
-// }
+console.log(countAndDescribe(['Sports', 'Cooking']))
