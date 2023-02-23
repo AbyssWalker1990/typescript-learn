c7c3140ce736fd22031b26f85e38b76a0674b577
Vova <bordiian.v@gmail.com>
2023-02-24 00:26:34 +0200

    module7 completed

--- dist/app.js
+++ dist/app.js
@@ -15,8 +15,32 @@ console.log(mergedObj)
 function countAndDescribe (element) {
   let descriptionText = 'Got No value'
   if (element.length > 0) {
-    descriptionText = 'Got ' + element.length + 'elements'
+    descriptionText = 'Got ' + String(element.length) + 'elements'
   }
   return [element, descriptionText]
 }
 console.log(countAndDescribe(['Sports', 'Cooking']))
+function extractAndConvert (obj, key) {
+  return obj[key]
+}
+extractAndConvert({ name: 'Lol' }, 'name')
+class DataStorage {
+  data = []
+  addItem (item) {
+    this.data.push(item)
+  }
+
+  removeItem (item) {
+    this.data.splice(this.data.indexOf(item), 1)
+  }
+
+  getItems () {
+    return [...this.data]
+  }
+}
+const textStorage = new DataStorage()
+textStorage.addItem('Doll')
+textStorage.addItem('King')
+textStorage.removeItem('Doll')
+console.log(textStorage)
+const numberStorage = new DataStorage()
--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@
 //   data.split(' ')
 // })
 
-function merge<T extends object, U extends object> (objA: T, objB: U) {
+function merge<T extends object, U extends object> (objA: T, objB: U): object {
   return Object.assign(objA, objB)
 }
 
@@ -24,9 +24,57 @@ interface Lengthy {
 function countAndDescribe<T extends Lengthy> (element: T): [T, string] {
   let descriptionText = 'Got No value'
   if (element.length > 0) {
-    descriptionText = 'Got ' + element.length + 'elements'
+    descriptionText = 'Got ' + String(element.length) + 'elements'
   }
   return [element, descriptionText]
 }
 
 console.log(countAndDescribe(['Sports', 'Cooking']))
+
+function extractAndConvert<T extends object, U extends keyof T> (obj: T, key: U): T[U] {
+  return obj[key]
+}
+
+extractAndConvert({ name: 'Lol' }, 'name')
+
+class DataStorage<T extends string | number | boolean> {
+  private readonly data: T[] = []
+
+  addItem (item: T): void {
+    this.data.push(item)
+  }
+
+  removeItem (item: T): void {
+    this.data.splice(this.data.indexOf(item), 1)
+  }
+
+  getItems (): T[] {
+    return [...this.data]
+  }
+}
+
+const textStorage = new DataStorage<string>()
+textStorage.addItem('Doll')
+textStorage.addItem('King')
+textStorage.removeItem('Doll')
+console.log(textStorage)
+
+const numberStorage = new DataStorage<number>()
+numberStorage.addItem(1)
+
+interface CourseGoal {
+  title: string
+  description: string
+  completeUntil: Date
+}
+
+function createCourseGoal (title: string, description: string, date: Date): CourseGoal {
+  const courseGoal: Partial<CourseGoal> = {}
+  courseGoal.title = title
+  courseGoal.description = description
+  courseGoal.completeUntil = date
+  return courseGoal as CourseGoal
+}
+
+const names: Readonly<string[]> = ['Max', 'Anna']
+names.push('Manu')
