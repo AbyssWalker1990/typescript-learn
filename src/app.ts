c428252a6250d682a8f72f60ba21b0a96fbb34b5
Vova <bordiian.v@gmail.com>
2023-02-25 01:02:40 +0200

    module8 completed

--- src/app.ts
+++ src/app.ts
@@ -94,3 +94,105 @@ class Product {
     return this.price * (1 + tax)
   }
 }
+
+const p1 = new Product('Book', 19)
+const p2 = new Product('Book 2', 29)
+
+function Autobind (_: any, _2: string, descriptor: PropertyDescriptor): PropertyDescriptor {
+  const origonalMethod = descriptor.value
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get () {
+      const boundFn = origonalMethod.bind(this)
+      return boundFn
+    }
+  }
+  return adjDescriptor
+}
+
+class Printer {
+  message = 'This Works!'
+
+  @Autobind
+  showMessage (): void {
+    console.log(this.message)
+  }
+}
+
+const p = new Printer()
+
+const button: HTMLButtonElement = document.querySelector('button')!
+button.addEventListener('click', p.showMessage)
+
+// ----
+
+type ValidatorConfig = Record<string, Record<string, string[]>>
+
+const registeredValidators: ValidatorConfig = {}
+
+function Required (target: any, propName: string): void {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: [...(registeredValidators[target.constructor.name]?.[propName] ?? []), 'required']
+  }
+}
+
+function PositiveNumber (target: any, propName: string): void {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: [...(registeredValidators[target.constructor.name]?.[propName] ?? []), 'positive']
+  }
+}
+
+function validate (obj: any): boolean {
+  const objValidatorConfig = registeredValidators[obj.constructor.name]
+  if (!objValidatorConfig) {
+    return true
+  }
+  let isValid = true
+  for (const prop in objValidatorConfig) {
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case 'required':
+          isValid = isValid && !!obj[prop]
+          break
+        case 'positive':
+          isValid = isValid && obj[prop] > 0
+          break
+      }
+    }
+  }
+  return isValid
+}
+
+class Course {
+  @Required
+    title: string
+
+  @PositiveNumber
+    price: number
+
+  constructor (t: string, p: number) {
+    this.title = t
+    this.price = p
+  }
+}
+
+const courseForm = document.querySelector('form')!
+courseForm.addEventListener('submit', event => {
+  event.preventDefault()
+  const titleEl = document.getElementById('title') as HTMLInputElement
+  const priceEl = document.getElementById('price') as HTMLInputElement
+
+  const title = titleEl.value
+  const price = +priceEl.value
+
+  const createdCourse = new Course(title, price)
+
+  if (!validate(createdCourse)) {
+    alert('Invalid Input!!!')
+    return
+  }
+  console.log(createdCourse)
+})
